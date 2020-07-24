572e831dec654e18a7e9d7c331662caa100d442b
Yuburov <yubur.aziz@gmail.com>
2020-07-24 11:01:10 +0600

    Авторизация

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Header from "./components/Header/Header";
 import Navbar from "./components/Navbar/Navbar";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
@@ -9,13 +8,14 @@ import {Route} from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
 import ProfileContainer from "./components/Profile/ProfileContainer";
+import HeaderComponent from "./components/Header/HeaderContainer";
 
 
 
 function App(props) {
   return (
         <div className="app-wrapper">
-          <Header/>
+          <HeaderComponent/>
           <Navbar/>
           <div className='app-wrapper-content'>
             <Route path='/dialogs' render={() => <DialogsContainer/>}/>
